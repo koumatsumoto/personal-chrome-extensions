79a37c7ec242e7ae978ca20bbca0eaf2ce02f5f8
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-09-07 20:56:00 +0900

    save old changes

--- src/background.ts
+++ src/background.ts
@@ -1,38 +1,67 @@
-import { receiveComment } from './internal/comments/comments';
+const websocketUrl = 'wss://scrapbox.io/socket.io/?EIO=3&transport=websocket';
+const loginCheckApiUrl = 'https://scrapbox.io/api/users/me';
+const originUrl = 'https://scrapbox.io';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log('The color is green.');
-  });
-});
+const aa = async () => {
+  try {
+    const res = await fetch(loginCheckApiUrl);
+    const data = await res.json();
+    alert(JSON.stringify(data, null, 2));
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-  chrome.declarativeContent.onPageChanged.addRules([
-    {
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {
-            schemes: ['http', 'https'],
-          },
-        }),
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()],
-    },
-  ]);
-});
+    const socket = new WebSocket(websocketUrl);
+    socket.addEventListener(
+      'open',
+      () => {
+        console.log('[websocket] open');
+      },
+      { once: true },
+    );
+    // reconnect on error and close
+    socket.addEventListener('close', () => console.log('[websocket] close'), { once: true });
+    socket.addEventListener('error', () => console.log('[websocket] error'), { once: true });
+    socket.addEventListener('message', (ev: MessageEvent) => console.log('[websocket] message'));
+  } catch (e) {
+    alert(JSON.stringify(e, null, 2));
+  }
+};
 
-receiveComment();
+const getHeaderValue = (headers: chrome.webRequest.HttpHeader[], name: string) => {
+  const target = headers.find((h) => h.name === name);
+
+  return target ? target.value : undefined;
+};
+
+const setHeader = (headers: chrome.webRequest.HttpHeader[], name: string, value: string) => {
+  const target = headers.find((h) => h.name === name);
+  if (target) {
+    target.value = value;
+  } else {
+    headers.push({ name, value });
+  }
+};
+
+let cookieFromHeader: string | undefined;
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   (details) => {
-    for (let i = 0; i < details.requestHeaders!.length; ++i) {
-      if (details.requestHeaders![i].name === 'Origin') {
-        details.requestHeaders!.splice(i, 1);
-        break;
-      }
+    console.log('[dev] details', details);
+    const headers = details.requestHeaders || [];
+
+    if (details.url === loginCheckApiUrl) {
+      cookieFromHeader = getHeaderValue(headers, 'Cookie');
     }
-    return { requestHeaders: details.requestHeaders };
+
+    if (details.url === websocketUrl && cookieFromHeader) {
+      setHeader(headers, 'Origin', originUrl);
+      setHeader(headers, 'Cookie', cookieFromHeader);
+    }
+
+    return { requestHeaders: headers };
   },
-  { urls: ['*://scrapbox.io/'] },
-  ['blocking', 'requestHeaders'],
+  { urls: ['wss://scrapbox.io/*', 'https://scrapbox.io/*'], types: ['xmlhttprequest', 'websocket'] },
+  ['requestHeaders', 'blocking', 'extraHeaders'],
 );
+
+chrome.runtime.onInstalled.addListener(() => {
+  setTimeout(() => aa(), 5000);
+});
